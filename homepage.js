16fb753280743c78abc851d50ec12868b336570d
Ryan Gao <Ryan@Ryans-MacBook-Pro-4.local>
2017-10-31 17:31:34 -0400

    Changed front-end and implemented part of the search bar functionality

--- homepage.js
+++ homepage.js
@@ -1,29 +1,39 @@
 var app = angular.module('myApp', []);
 
 app.controller('homepageController', function($scope, $timeout) {
+    $scope.currentCurrency = 'ETH';
     $scope.getPriceInfo = function(currencyFrom, currencyTo) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR");
+        xhr.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=" + currencyFrom + "&tsyms=BTC,USD,EUR");
         //https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=BTC,USD,EUR
         xhr.onload = function () {
             var result = JSON.parse(xhr.response);
             $scope.price = result[currencyTo];
+            //setTwoNumberDecimal();
             $scope.showRefresh = false;
             $timeout(function() {
                 $scope.showRefresh = true;
-             }, 3000);
-            console.log($scope.price);
+            }, 3000);
+            // console.log($scope.price);
             $scope.$digest()
-          };
-          xhr.onerror = function (e) {
+        };
+        xhr.onerror = function (e) {
             console.error(xhr.statusText);
-          };
+        };
         xhr.send();
-      }
-      $scope.searchFunc = function() {
+    }
+    $scope.searchFunc = function() {
         console.log("made");
-      }
-    $scope.getPriceInfo('ETH', 'USD');
+    }
+    $scope.changeCurrency = function() {
+        $scope.currentCurrency = $scope.searchText.toUpperCase();
+        $scope.getPriceInfo($scope.currentCurrency, 'USD');
+        $scope.searchText = '';
+    }
+      /*function setTwoNumberDecimal() {
+        $scope.price = parseFloat($scope.price).toFixed(2);
+        }*/
+    $scope.getPriceInfo($scope.currentCurrency, 'USD');
     //$scope.getPriceInfo('USD');
     //$scope.price = 4;
 });
